611e0342b88a10b76f32a2f209e2ccd0eb7648f7
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-09 16:31:58 -0500

    added randomLength function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
+var randomLength = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
+}
 
+var generatePassword = function (passwordLength, characters) {
+  var passwordLength = randomLength(8, 128);
+  let characters = ["A", "B", "C"]
+  let password = "";
+  for (var i = 0; i <= passwordLength, i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    )
+  }
+  return password;
+}
+
+console.log(password);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
