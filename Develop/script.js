9f490b13b905adb9a4e51be2e9b031cfb98c56bd
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-09 22:09:18 -0500

    compile array with prompts, password array

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,49 @@
-// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+//Assignment code
+//Add generatePassword function
 function generatePassword() {
 
-//Make an empty array to compile charachters
-var compileArray = []
+//Define a compile array and array for password characters
+var compileArray = [];
+var passwordArray = [];
 
-//Make an array to compile the charachters of the password
-var passwordArray = []
-
-//Add arrays for each set of charachters
+//Define character arrays
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var charactersArray = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-//Add the prompt for character count
-var characterCount = prompt ("How many Characters do you want your password to be (between 8 and 128)?");
-
-//Prompt for lowercase characters
-var lowercase = confirm ("Do you want to include lowercase characters in your password?");
-
-//Prompt for uppercase characters
-var uppercase = confirm ("Do you want to include uppercase characters in your password?");
-
-//Prompt for numbers
-var numbers = confirm ("Do you want numbers in your password?");
-
-//Prompt for special characters
-var characters = confirm ("Do you want special characters in your password?");
+//Set prompts
+var characterCount = prompt ("How many number characters do you want your password to be (between 8 and 128)?");
+var numbers = confirm ("Do you want your password to include numbers?");
+var uppercase = confirm ("Do you want your password to include uppercase letters?");
+var lowercase = confirm ("Do you want your password to include lowercase letters?");
+var character = confirm ("Do you want your password to include special characters?");
 
 
+//set compileArray with conditions
+if (numbers){
+  compileArray = compileArray.concat(numberArray); 
+}
+if (uppercase){
+  compileArray = compileArray.concat(uppercaseArray);
+}
+if (lowercase){
+  compileArray = compileArray.concat(lowercaseArray);
+}
+if (character){
+  compileArray = compileArray.concat(charactersArray);
 }
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//generate passwordArray and join
+for (var i = 0; i < characterCount; i++) {
+      
+  passwordArray.push (compileArray[Math.floor(Math.random() * compileArray.length)]); 
+  
+}
+  return passwordArray.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
