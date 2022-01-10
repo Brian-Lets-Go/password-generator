878f2d9106611985bd04709a058b394d23e631cc
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-09 21:33:38 -0500

    add arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment code here
+function generatePassword() {
 
+//Make an empty array to compile charachters
+var compileArray = []
 
+//Make an array to compile the charachters of the password
+var passwordArray = []
+
+//Add arrays for each set of charachters
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+}
 
 
 // Get references to the #generate element
